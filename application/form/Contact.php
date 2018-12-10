fd2d6aff016a5d610f206cf4de8079dd1ad37eb6
Larisse Alves <alves.larisser@gmail.com>
2018-12-10 14:33:01 -0300

    Formulário usando Zend Framework e Bootstrap

--- application/form/Contact.php
+++ application/form/Contact.php
@@ -1,11 +1,64 @@
 <?php
 class Application_Form_Contact extends Zend_Form{
-
 	public function init()
 	{
-		$this->addElement('text','name', array('label'=>'Digite seu nome: ','required'=>true));
-		$this->addElement('text','contact', array('label'=>'Telefone para Contato: ', 'required'=>true));
-		$this->addElement('submit','submit', array('label'=> 'Adicionar Contato'));
+
+	 	$this->setDecorators(array( 'FormElements', 'Form'));
+        $this->setMethod('post');
+        $this->setAttrib('class', 'form-horizontal');
+
+		//$this->addElement('text','name', array('label'=>'Digite seu nome: ','required'=>true));
+		//$this->addElement('text','contact', array('label'=>'Telefone para Contato: ', 'required'=>true));
+		//$this->addElement('submit','submit', array('label'=> 'Adicionar Contato'));
+
+		$name = new Zend_Form_Element_Text('name');
+        $name -> clearDecorators();
+        $name->setAttrib('class', 'form-control');
+        $name -> addDecorators($this->getBootstrapDecorator());
+        $name->setLabel("Nome:")->setRequired(true);
+
+        $contact = new Zend_Form_Element_Text('contact');
+        $contact -> clearDecorators();
+        $contact->setAttrib('class', 'form-control');
+        $contact -> addDecorators($this->getBootstrapDecorator());
+        $contact->setLabel("contact:")->setRequired(true);
+
+
+        $submit = new Zend_Form_Element_Submit('relatorio');
+        $submit->setLabel('Pesquisar');
+        $submit->setAttrib('class', 'btn btn-primary');
+        $submit -> setAttrib('id', 'submitbutton');
+        $submit->setAttrib('style', 'float:right;');
+        $submit -> clearDecorators();
+        $submit -> setDecorators(array('ViewHelper'));
+
+
+		$this->addElements(array($name, $contact, $submit));
 
 	}
+
+	private function getBootstrapDecorator()
+    {
+        return array(
+            'ViewHelper',
+            'Errors',
+            'Description',
+
+            array(
+                array('col' => 'HtmlTag'), array('tag' => 'div','class' => 'col-sm-6'),
+            ),
+            array(
+                'Label', array('class' => 'control-label col-sm-2')
+            ),
+            array(
+                array('form-group' => 'HtmlTag'), array('tag' => 'div','class' => 'form-group')
+            )
+
+        );
+
+    }
+
+
 }
+
+
